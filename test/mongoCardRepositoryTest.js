6628ef739212758a741d9550c4c4d86559e728d8
kwasniew <kwasniewski.mateusz@gmail.com>
2021-08-12 12:13:43 +0200

    mongo repo test

---
+++ test/mongoCardRepositoryTest.js
@@ -0,0 +1,34 @@
+const test = require('tape');
+const cardFactory = require('../src/card');
+const cardRepository = require('../src/mongoCardRepository');
+const init = require('../src/es');
+function now() { return new Date('August 19, 2018 23:15:30 UTC'); }
+const {card, recreateFrom} = cardFactory(now);
+
+test("should be able to save and load credit card", async function (t) {
+    const es = await init();
+    const repository = cardRepository(recreateFrom, es);
+    let c = card(Math.random()+"");
+
+    c.assignLimit(100);
+    c.withdraw(20);
+    c.repay(20);
+
+    t.equal(c.pendingEvents().length, 3);
+    await repository.save(c);
+    t.equal(c.pendingEvents().length, 0);
+
+    c = await repository.load(c.uuid());
+    t.equal(c.availableLimit(), 100);
+
+    c.withdraw(100);
+
+    t.equal(c.pendingEvents().length, 1);
+    await repository.save(c);
+    t.equal(c.pendingEvents().length, 0);
+
+    c = await repository.load(c.uuid());
+    t.equal(c.availableLimit(), 0);
+
+    t.end();
+});
